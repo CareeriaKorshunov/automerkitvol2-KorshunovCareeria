8c71506efa43fe431e5d99670bcbb0131fb2494e
KorshunovCareeria <158146367+KorshunovCareeria@users.noreply.github.com>
2024-10-07 08:58:20 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,7 +5,21 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-           
+                List<string> autolista = new List<string>();
+            string syote = "";
+
+            Console.WriteLine("Lisää listalle viisi   automerkkiä");
+
+            while (true)
+            {
+                syote = Console.ReadLine();
+                if (syote == "X" || syote == "x")
+                {
+                    break;
+                }
+                autolista.Add(syote);
+            }
+            Console.WriteLine("Kiitos listan täyttämisestä!");
         }
     }
 }
